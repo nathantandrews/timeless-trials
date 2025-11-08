954478082a9aa2562a26ce94e6d2d835b6c71a88
Ryan Sahyoun <sahyounr@uci.edu>
2025-11-08 14:29:40 -0800

    level 0 update time out ends game, need other implementations

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,40 +1,132 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    GameManager instance;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public static GameManager Instance;
+
+    [Header("UI Panels")]
+    public GameObject victoryPanel = null;
+    public GameObject deathPanel = null;
+
+    [Header("References")]
+    public GameTimer gameTimer = null; // Assign your GameTimer script here
+
+    private bool isGameOver = false;
+
+    void Awake()
+    {
+        // Simple singleton pattern
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
     void Start()
     {
-        instance = this;
-    }    
+        HideAllPanels();
+        ResumeGame(); // make sure time scale is normal
+    }
+
+    void Update()
+    {
+        // Escape key: go back to Main Menu directly
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            GoToMainMenu();
+        }
+
+        // If timer hits zero and the game isn't already over
+        if (gameTimer != null && gameTimer.GetCurrentTime() <= 0 && !isGameOver)
+        {
+            EndLevel(false);
+        }
+    }
+
+    // ------------------- Scene Navigation -------------------
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 1f; // reset just in case
+        SceneManager.LoadScene("MainMenu");
+    }
+
     public void GoToLevelSelect()
     {
-        SceneManager.LoadScene("LevelSelect"); // The name must match your scene name!
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("LevelSelect");
     }
 
     public void GoToOptions()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene("Options");
     }
 
     public void ExitGame()
     {
-        #if UNITY_EDITOR
-                UnityEditor.EditorApplication.isPlaying = false;  // Stop play mode if in editor
-        #else
-                Application.Quit(); // Quit the app if built
-        #endif
-    }
-    public void StartLevel(int levelId)
-    {
-        return;
-    }
-    public void EndLevel(bool passedLevel)
-    {
-        return;
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
     }
 
+    public void StartLevel(int levelId)
+    {
+        SceneManager.LoadScene("Level" + levelId);
+    }
+
+    // ------------------- Game Logic -------------------
+    public void EndLevel(bool passedLevel)
+    {
+        if (isGameOver) return;
+
+        isGameOver = true;
+        FreezeGame();
+
+        if (passedLevel)
+        {
+            ShowVictoryPanel();
+        }
+        else
+        {
+            ShowDeathPanel();
+        }
+    }
+
+    private void FreezeGame()
+    {
+        Time.timeScale = 0f;
+    }
+
+    private void ResumeGame()
+    {
+        isGameOver = false;
+        Time.timeScale = 1f;
+        HideAllPanels();
+    }
+
+    private void HideAllPanels()
+    {
+        if (victoryPanel != null) victoryPanel.SetActive(false);
+        if (deathPanel != null) deathPanel.SetActive(false);
+    }
+
+    private void ShowVictoryPanel()
+    {
+        HideAllPanels();
+        if (victoryPanel != null) victoryPanel.SetActive(true);
+    }
+
+    private void ShowDeathPanel()
+    {
+        HideAllPanels();
+        if (deathPanel != null) deathPanel.SetActive(true);
+    }
 }
