d338abf6dbfda779881c753821d1f3d5b202f574
Ryan Sahyoun <sahyounr@uci.edu>
2025-11-08 11:47:30 -0800

    Basic UI controls for Level Selection, Options, Exiting Game, and Returning to Main Menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,4 +8,18 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene("LevelSelect"); // The name must match your scene name!
     }
+
+    public void GoToOptions()
+    {
+        SceneManager.LoadScene("Options");
+    }
+
+    public void ExitGame()
+    {
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;  // Stop play mode if in editor
+        #else
+                Application.Quit(); // Quit the app if built
+        #endif
+    }
 }
